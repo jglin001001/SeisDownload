b45ee9825bf7ca52b7072738142d0877397ba388
Jungle LIN <jglin001001@foxmail.com>
2023-11-29 19:30:37 +0800

    Add files via upload

---
+++ filter_seis.py
@@ -0,0 +1,8 @@
+import obspy
+
+st = obspy.read('Seis_mseed_download/4_XF_B01.mseed')
+
+#  复制原始stream后对其进行带通滤波
+st_filt = st.copy()
+tr = st_filt.filter('bandpass',freqmin=0.03,freqmax=2,corners=4,zerophase=True)
+tr.plot()
