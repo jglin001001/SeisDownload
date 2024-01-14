c0e9a0ccc58b8643989eb29415a8cda61fb8e972
Jungle LIN <jglin001001@foxmail.com>
2024-01-14 18:21:49 +0800

    Delete 5_filter_seis.py

--- 5_filter_seis.py
+++
@@ -1,8 +0,0 @@
-import obspy
-
-st = obspy.read('Seis_mseed_download_station/1_XF_B01.mseed')
-
-#  复制原始stream后对其进行带通滤波
-st_filt = st.copy()
-tr = st_filt.filter('bandpass',freqmin=0.03,freqmax=2,corners=4,zerophase=True)
-tr.plot(equal_scale=False)
